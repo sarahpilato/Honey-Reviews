f25a719d8a35a8a5a511099dc0fc4088a5b95a23
sarahpilato <sarahkitap@gmail.com>
2021-03-05 14:37:13 -0500

    first commit of starting files

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Honey</h1>
+      <h2>🍯 sweet skincare reviews🍯</h2>
     </div>
   );
 }
